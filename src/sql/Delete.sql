8e2612f780aedf428a4ecc03ea589200caeca5d9
JiaqiWang426 <jw7859@nyu.edu>
2025-11-07 20:18:28 -0500

    Add files via upload

---
+++ src/sql/Delete.sql
@@ -0,0 +1,17 @@
+USE inventory_management;
+
+DELETE FROM loan_details;
+
+DELETE FROM loans;
+
+DELETE FROM assets;
+
+DELETE FROM items;
+
+DELETE FROM categories_children;
+
+DELETE FROM categories;
+
+DELETE FROM locations;
+
+DELETE FROM users;
---
+++ src/sql/UPDATE.sql
@@ -0,0 +1,88 @@
+DELIMITER //
+
+CREATE PROCEDURE sp_update_user_info(
+    IN p_u_id INT,
+    IN p_fname VARCHAR(20),
+    IN p_lname VARCHAR(20),
+    IN p_email VARCHAR(20),
+    IN p_role  VARCHAR(12),
+    IN p_active BOOLEAN
+)
+BEGIN
+    UPDATE users
+    SET u_fname = p_fname,
+        u_lname = p_lname,
+        u_email = p_email,
+        u_role  = p_role,
+        u_active = p_active
+    WHERE u_id = p_u_id;
+END//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE sp_move_item_to_category(
+    IN p_it_id INT,
+    IN p_new_cat_id INT
+)
+BEGIN
+    UPDATE items
+       SET cat_id = p_new_cat_id
+     WHERE it_id  = p_it_id;
+END//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE sp_set_category_parent(
+    IN p_cat_id INT,
+    IN p_new_parent_id INT 
+)
+BEGIN
+    UPDATE categories
+       SET cat_parent_id = p_new_parent_id
+     WHERE cat_id = p_cat_id;
+END//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE sp_update_asset(
+    IN p_a_id INT,
+    IN p_new_status VARCHAR(20),     
+    IN p_new_condition VARCHAR(20),  
+    IN p_new_loc_id INT              
+)
+BEGIN
+    UPDATE assets
+       SET a_status    = p_new_status,
+           a_condition = p_new_condition,
+           loc_id      = p_new_loc_id
+     WHERE a_id        = p_a_id;
+END//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE sp_checkout_updates(
+    IN p_l_id INT,  
+    IN p_a_id INT    
+)
+BEGIN
+    START TRANSACTION;
+    UPDATE assets
+       SET a_status = 'checked_out'
+     WHERE a_id = p_a_id;
+    UPDATE loans
+       SET l_status = 'open'
+     WHERE l_id = p_l_id;
+    COMMIT;
+END//
+
+DELIMITER ;
+
+SHOW PROCEDURE STATUS WHERE Db = DATABASE();
